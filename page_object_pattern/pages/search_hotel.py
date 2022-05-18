0c314744e41dcba4535deac831af1a2ed691693e
karolina-meier <meier.karolinaa@gmail.com>
2022-05-18 10:42:44 +0200

    Adding logs

--- page_object_pattern/pages/search_hotel.py
+++ page_object_pattern/pages/search_hotel.py
@@ -1,21 +1,26 @@
 from selenium.webdriver.common.by import By
 from page_object_pattern.locators.locators import SearchHotelLocators
+import logging
 
 class SearchHotelPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.logger = logging.getLogger()
 
     def set_city(self, city):
+        self.logger.info("Setting city {}".format(city))
         self.driver.find_element(By.XPATH, SearchHotelLocators.search_hotel_span_xpath).click()
         self.driver.find_element(By.XPATH, SearchHotelLocators.search_hotel_input_xpath).send_keys(city)
         self.driver.find_element(By.XPATH, SearchHotelLocators.location_match_xpath).click()
 
     def set_date_range(self, check_in, check_out):
+        self.logger.info("Setting date range {} - {}".format(check_in, check_out))
         self.driver.find_element(By.NAME, SearchHotelLocators.check_in_input_name).send_keys(check_in)
         self.driver.find_element(By.NAME, SearchHotelLocators.check_out_input_name).send_keys(check_out)
 
     def set_travellers(self, adults, child):
+        self.logger.info("Setting travellers: {} adults and {} child".format(adults, child))
         self.driver.find_element(By.ID, SearchHotelLocators.travellers_input_id).click()
         self.driver.find_element(By.ID, SearchHotelLocators.adult_input_id).clear()
         self.driver.find_element(By.ID, SearchHotelLocators.adult_input_id).send_keys(adults)
@@ -23,4 +28,5 @@ class SearchHotelPage:
         self.driver.find_element(By.ID, SearchHotelLocators.child_input_id).send_keys(child)
 
     def perform_search(self):
+        self.logger.info("Performing search")
         self.driver.find_element(By.XPATH, SearchHotelLocators.search_btn_xpath).click()
--- page_object_pattern/pages/search_results.py
+++ page_object_pattern/pages/search_results.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from page_object_pattern.locators.locators import SearchResultLocators
+import logging
 
 class SearchResultsPage:
 
@@ -7,11 +8,20 @@ class SearchResultsPage:
         self.driver = driver
         self.hotel_names_xpath = SearchResultLocators.hotel_names_xpath
         self.hotel_prices_xpath = SearchResultLocators.hotel_prices_xpath
+        self.logger = logging.getLogger()
 
     def get_hotel_names(self):
         hotels = self.driver.find_elements(By.XPATH, self.hotel_names_xpath)
-        return [hotel.get_attribute("textContent") for hotel in hotels]
+        names = [hotel.get_attribute("textContent") for hotel in hotels]
+        self.logger.info("Available hotels are:")
+        for name in names:
+            self.logger.info(name)
+        return names
 
     def get_hotel_prices(self):
         prices = self.driver.find_elements(By.XPATH, self.hotel_prices_xpath)
-        return [price.get_attribute("textContent") for price in prices]
+        hotel_prices = [price.get_attribute("textContent") for price in prices]
+        self.logger.info("Hotel prices:")
+        for price in hotel_prices:
+            self.logger.info(price)
+        return hotel_prices
